aef486e6436503c2f9f56e70e0d54ef691e53f60
Christophe <cvauclair@protonmail.com>
2024-02-16 14:32:15 -0500

    style: Cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,8 @@ use anyhow::Result;
 use brc20::Brc20Event;
 use btc_utils::{btc_to_sats, parse_inscriptions};
 use pb::btc::brc20::v1::{
-    Brc20Events, Deploy, ExecutedTransfer, InscribedTransfer, InscribedTransferLocation, Mint, Token,
+    Brc20Events, Deploy, ExecutedTransfer, InscribedTransfer, InscribedTransferLocation, Mint,
+    Token,
 };
 use pb::sf::bitcoin::r#type::v1 as btc;
 use substreams::pb::substreams::store_delta::Operation;
@@ -44,13 +45,17 @@ fn map_brc20_events(block: btc::Block) -> Result<Brc20Events, substreams::errors
             match parse_inscriptions(&tx) {
                 Ok(inscriptions) => inscriptions
                     .into_iter()
-                    .filter(|inscription| match 
-                        inscription.content_type()
+                    .filter(|inscription| {
+                        match inscription
+                            .content_type()
                             .map(|ctype| ctype.split(";"))
                             .and_then(|mut parts| parts.next())
-                    {
-                        Some(content_type) => content_type == "text/plain" || content_type == "application/json",
-                        None => false,
+                        {
+                            Some(content_type) => {
+                                content_type == "text/plain" || content_type == "application/json"
+                            }
+                            None => false,
+                        }
                     })
                     .filter_map(|inscription| {
                         let (vout, offset) = tx.nth_sat_utxo(inscription.pointer().unwrap_or(0))?;
@@ -178,7 +183,6 @@ fn store_tokens(events: Brc20Events, store: StoreSetProto<Token>) {
     });
 }
 
-
 #[substreams::handlers::store]
 fn store_balances(events: Brc20Events, store: StoreAddBigInt) {
     // On mints, we add the amount to the receiver's balance
@@ -243,45 +247,49 @@ fn map_resolve_transfers(
     transfer_store: StoreGetProto<InscribedTransferLocation>,
     token_store: StoreGetProto<Token>,
 ) -> Result<Brc20Events, substreams::errors::Error> {
-    let executed_transfers = block
-        .tx
-        .into_iter()
-        .filter_map(|tx| {
-            // Note: Without tracking UTXO values, we can only reliably resolve transfers where the
-            // inscribed sat is held by the first input UTXO of the transaction
-            if let Some(inscribed_transfer_loc) =
-                transfer_store.get_at(0, format!("{}:{}", tx.vin[0].txid, tx.vin[0].vout))
-            {
-                let (vout, _) = tx.nth_sat_utxo(inscribed_transfer_loc.offset)?;
-                Some(ExecutedTransfer {
-                    id: inscribed_transfer_loc.id,
-                    token: inscribed_transfer_loc.token,
-                    from: inscribed_transfer_loc.from,
-                    to: vout.address()?,
-                    amount: inscribed_transfer_loc.amount,
-                })
-            } else {
-                // Log that we could not resolve transfer
-                if let Some(inscribed_transfer_loc) = tx
-                    .vin
-                    .iter()
-                    .find_map(|vin| transfer_store.get_at(0, format!("{}:{}", vin.txid, vin.vout)))
+    let executed_transfers =
+        block
+            .tx
+            .into_iter()
+            .filter_map(|tx| {
+                // Note: Without tracking UTXO values, we can only reliably resolve transfers where the
+                // inscribed sat is held by the first input UTXO of the transaction
+                if let Some(inscribed_transfer_loc) =
+                    transfer_store.get_at(0, format!("{}:{}", tx.vin[0].txid, tx.vin[0].vout))
                 {
-                    substreams::log::info!(
-                        "Could not resolve inscribed transfer {}",
-                        inscribed_transfer_loc.id
-                    );
+                    let (vout, _) = tx.nth_sat_utxo(inscribed_transfer_loc.offset)?;
+                    Some(ExecutedTransfer {
+                        id: inscribed_transfer_loc.id,
+                        token: inscribed_transfer_loc.token,
+                        from: inscribed_transfer_loc.from,
+                        to: vout.address()?,
+                        amount: inscribed_transfer_loc.amount,
+                    })
+                } else {
+                    // Log that we could not resolve transfer
+                    if let Some(inscribed_transfer_loc) = tx.vin.iter().find_map(|vin| {
+                        transfer_store.get_at(0, format!("{}:{}", vin.txid, vin.vout))
+                    }) {
+                        substreams::log::info!(
+                            "Could not resolve inscribed transfer {}",
+                            inscribed_transfer_loc.id
+                        );
+                    }
+                    None
                 }
-                None
-            }
-        })
-        .collect::<Vec<_>>();
+            })
+            .collect::<Vec<_>>();
 
     Ok(Brc20Events {
         executed_transfers,
-        mints: events.mints.into_iter()
+        mints: events
+            .mints
+            .into_iter()
             .filter(|mint| match token_store.get_at(0, mint.token.clone()) {
-                Some(token) => BigInt::from_str(&mint.amount).unwrap() < BigInt::from_str(&token.mint_limit).unwrap(),
+                Some(token) => {
+                    BigInt::from_str(&mint.amount).unwrap()
+                        < BigInt::from_str(&token.mint_limit).unwrap()
+                }
                 None => false,
             })
             .collect(),
@@ -304,9 +312,13 @@ fn graph_out(
             .set("token", deploy.symbol.clone())
             .set("deployer", deploy.deployer.clone())
             .set("timestamp", clock.number.clone())
-            .set("block", clock.timestamp.as_ref()
-                .map(|t| t.seconds)
-                .unwrap_or_default()
+            .set(
+                "block",
+                clock
+                    .timestamp
+                    .as_ref()
+                    .map(|t| t.seconds)
+                    .unwrap_or_default(),
             );
 
         tables
